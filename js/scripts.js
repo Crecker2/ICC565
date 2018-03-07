f8a75dbddab56fac0388870138cabaebc1829a72
Crecker2 <joshua.fayer@gmail.com>
2018-03-06 22:28:35 -0500

    upload site

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+window.sr = ScrollReveal();
+// sr.reveal('.cell', {origin: 'right', distance: '200px',});
+
+
+ $(".button-collapse").sideNav();
+ 
+// var source = document.getElementById('entry-template').innerHTML;
+// var template = Handlebars.compile(source);
+
+// function makeprofiles(){
+// 	$(guydata).each(function(){
+// 		var context = {name: this.name, bio: this.bio, picture: this.picture, part: this.part};
+// 		var html = template(context);
+// 		$('.guys').append(html);
+// 	});
+
+// }
